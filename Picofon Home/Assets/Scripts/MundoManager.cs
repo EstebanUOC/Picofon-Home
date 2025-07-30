c0bc5e06f1db007e4044cb4f42337058d46c7658
Kevin Giovanni Sánchez Robalino <2cv36985@gmail.com>
2025-07-29 19:13:40 -0500

    Agrego funcionalidad de navegación

---
+++ Picofon Home/Assets/Scripts/MundoManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MundoManager : MonoBehaviour
+{
+    public void IrAEscenaPrincipal()
+    {
+        SceneManager.LoadScene("EscenaPrincipal");
+    }
+
+}
+
