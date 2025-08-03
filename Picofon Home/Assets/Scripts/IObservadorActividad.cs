852252958f94b1f26a20ecf5b6f236f0e75e011e
Kevin Giovanni Sánchez Robalino <2cv36985@gmail.com>
2025-08-03 16:52:33 -0500

    Creo archivos y carpetas en blanco para aplicar los patrones

--- Picofon Home/Assets/Scripts/IObservadorActividad.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IObservadorActividad
-{
-    void OnActividadCompletada(Actividad actividad, Usuario usuario);
-}
-
