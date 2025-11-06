f4db7a6763cac5181459452f49ef4b6055ee4cfe
RanMd <randommid1873@gmail.com>
2025-11-06 18:59:28 -0500

    feat(map): add parallax background

--- Picofon Home/Assets/Scripts/UI/SceneOrientationManager.cs
+++ Picofon Home/Assets/Scripts/UI/SceneOrientationManager.cs
@@ -7,7 +7,7 @@ public class SceneOrientationManager : MonoBehaviour
     {
         string sceneName = SceneManager.GetActiveScene().name;
 
-        if (sceneName == "LoginScene")
+        if (sceneName == "LoginScene" || sceneName == "MapScene")
         {
             // Enable autorotation
             Screen.autorotateToLandscapeLeft = false;
