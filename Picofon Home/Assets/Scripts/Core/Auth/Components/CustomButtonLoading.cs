2b4644c78b37909ddad76db3d7638368ac30d6cf
RanMd <randommid1873@gmail.com>
2025-12-02 10:14:19 -0500

    feat(ui): add button loading

--- Picofon Home/Assets/Scripts/Core/Auth/Components/CustomButtonLoading.cs
+++ Picofon Home/Assets/Scripts/Core/Auth/Components/CustomButtonLoading.cs
@@ -6,17 +6,73 @@ public class CustomButtonLoading : CustomButtonRaised
 {
     [Space(15)]
     public RectTransform LoadingRect;
+    public CanvasGroup InfoCanvasGroup;
+    public CanvasGroup LoadingCanvasGroup;
+
+    private Sequence fadeSequence;
+    private Tween loadingTween;
+
+    private bool _loading = false;
+    private bool Loading
+    {
+        get => _loading;
+        set
+        {
+            _loading = value;
+
+            if (_loading)
+            {
+                InfoCanvasGroup.alpha = 0;
+                fadeSequence.Restart();
+
+                loadingTween.Restart();
+            }
+            else
+            {
+                fadeSequence.PlayBackwards();
+                loadingTween.Pause();
+                InfoCanvasGroup.alpha = 1;
+            }
+        }
+    }
 
     public void Start()
     {
-        LoadingRect
+        fadeSequence = DOTween.Sequence().SetAutoKill(false).Pause();
+
+        fadeSequence
+            .Append(ContentCanvasGroup.DOFade(0.6f, Duration))
+            .Append(LoadingCanvasGroup.DOFade(1, Duration));
+
+        loadingTween = LoadingRect
             .DORotate(new Vector3(0, 0, -360), 1f, RotateMode.FastBeyond360)
             .SetLoops(-1, LoopType.Restart)
-            .SetEase(Ease.Linear);
+            .SetEase(Ease.Linear)
+            .SetAutoKill(false)
+            .Pause();
     }
 
     public override void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("CustomButtonLoading Clicked - Ignored during loading");
+        if (Loading)
+            return;
+
+        Loading = true;
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        if (Loading)
+            return;
+
+        base.OnPointerEnter(eventData);
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        if (Loading)
+            return;
+
+        base.OnPointerExit(eventData);
     }
 }
