65827ebb65be1a6eda99f4b7ecfd84588805d901
RanMd <randommid1873@gmail.com>
2025-12-09 15:12:47 -0500

    feat(form): add CenterField component

---
+++ Picofon Home/Assets/Scripts/Core/Auth/Inputs/CenterField.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CenterInput : FormInput
+{
+    [Space(15)]
+    public Toggle Toggle;
+
+    public void Start()
+    {
+        Toggle.onValueChanged.AddListener(OnToggleChange);
+    }
+
+    public override string GetData()
+    {
+        // TODO: Replace with actual center ID retrieval logic
+        int center = 1;
+        return center.ToString();
+    }
+
+    private void OnToggleChange(bool isOn)
+    {
+        Valid = isOn;
+    }
+}
