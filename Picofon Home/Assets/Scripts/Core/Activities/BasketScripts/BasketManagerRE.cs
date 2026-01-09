ba9f60f5831b998bbebdb234f1d4da2bfa55462a
RanMd <randommid1873@gmail.com>
2026-01-09 17:25:45 -0500

    feat(basket): enhance imagen tween

---
+++ Picofon Home/Assets/Scripts/Core/Activities/BasketScripts/BasketManagerRE.cs
@@ -0,0 +1,162 @@
+using System;
+using BasketResponses;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using ActivitiesResult = BasketResponses.ApiResult<BasketResponses.ActivitiesData<BasketResponses.RelateActivity>>;
+
+public class BasketManagerRE : MonoBehaviour
+{
+    [Space(15)]
+    [SerializeField]
+    private BallController _ballController;
+
+    [SerializeField]
+    private FeedbackController _feedbackController;
+
+    [Space(15)]
+    [SerializeField]
+    private AnswerManagerPS _answerManager;
+
+    [SerializeField]
+    private HoopManager _hoopManager;
+
+    [SerializeField]
+    private BasketUIManager _uiManager;
+
+    [SerializeField]
+    private AudioClip _instructionClip;
+
+    [SerializeField]
+    private AudioClip _positiveClip;
+
+    [SerializeField]
+    private AudioClip _negativeClip;
+
+    private int _currentActivityIndex = 0;
+
+    private RelateActivity[] _activities;
+    private RelateActivity _currentActivity;
+
+    private readonly Sprite[] _icons = new Sprite[4];
+    private readonly string[] _texts = new string[4];
+    private readonly string[] _syllabifiedWords = new string[4];
+
+    public void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+        _answerManager.OnHoopSelected += HandleHoopSelected;
+
+        _feedbackController.Init();
+
+        LoadActivities().Forget();
+    }
+
+    private async UniTaskVoid LoadActivities()
+    {
+        BasketService basketService = new();
+
+        ActivityRequestParams @params = new() { PlanId = "36", ChildId = "98765432M" };
+
+        ActivitiesResult result = await basketService.GetActivitiesP<
+            ActivitiesData<RelateActivity>
+        >(@params);
+
+        // NOTE: Wait a frame to ensure all initializations are done, Do not delete, 100% necessary
+        await UniTask.Yield();
+
+        if (!result.Success)
+        {
+            Debug.LogError($"Error loading activities: {result.Message}");
+            return;
+        }
+
+        _activities = result.Data.Activities;
+
+        if (_activities is null || _activities.Length == 0)
+        {
+            Debug.LogWarning("No hay actividades cargadas.");
+            return;
+        }
+
+        ChangeActivity();
+
+        AudioManager.Instance.PlayVoice(_instructionClip);
+        await AudioManager.Instance.WaitVoiceToEnd();
+    }
+
+    private void HandleHoopSelected(int hoopIndex)
+    {
+        Transform hoopTransform = _hoopManager.GetHoopTransform(hoopIndex);
+
+        _ballController.LaunchBall(hoopTransform);
+
+        bool isCorrect = _currentActivity.Words[hoopIndex].Answer;
+
+        AudioClip clip = isCorrect ? _positiveClip : _negativeClip;
+
+        AudioManager.Instance.PlayVoice(clip);
+
+        InitCount(isCorrect).Forget();
+    }
+
+    private async UniTaskVoid InitCount(bool isCorrect)
+    {
+        FeedbackType feedbackType = isCorrect ? FeedbackType.Positive : FeedbackType.Neutral;
+
+        await UniTask.WaitForSeconds(2f);
+
+        await _feedbackController.ShowFeedback(feedbackType);
+
+        ChangeActivity();
+    }
+
+    private void ChangeActivity()
+    {
+        _currentActivity = _activities[_currentActivityIndex];
+        _currentActivityIndex = (_currentActivityIndex + 1) % _activities.Length;
+
+        for (int i = 0; i < _texts.Length; i++)
+        {
+            _texts[i] = _currentActivity.Words[i].Word;
+            _icons[i] = LoadSprite(_currentActivity.Words[i].Path);
+            _syllabifiedWords[i] = _currentActivity.Words[i].SyllabifiedWord;
+        }
+
+        Span<bool> answers = stackalloc bool[4];
+        for (int i = 0; i < answers.Length; i++)
+        {
+            answers[i] = _currentActivity.Words[i].Answer;
+        }
+
+        AnswerDTO answer = new(answers);
+        _hoopManager.SetHoopStates(in answer);
+
+        ViewContentDTO content = new(_icons, _texts);
+
+        _uiManager.SetViewContent(in content);
+
+        ViewContentDTO feedbackContent = new(_icons, _syllabifiedWords);
+
+        _feedbackController.SetItemsContent(in feedbackContent);
+
+        ResetActivity();
+    }
+
+    private void ResetActivity()
+    {
+        _uiManager.Reset();
+        _ballController.Reset();
+    }
+
+    private Sprite LoadSprite(string p)
+    {
+        string file = System.IO.Path.GetFileNameWithoutExtension(p);
+        Sprite s = Resources.Load<Sprite>($"Images/ImgButtons/{file}");
+
+        if (!s)
+            Debug.LogWarning($"No se encontró sprite: {file}");
+
+        return s;
+    }
+}
--- Picofon Home/Assets/Scripts/Core/Activities/BasketScripts/Services/BasketService.cs
+++ Picofon Home/Assets/Scripts/Core/Activities/BasketScripts/Services/BasketService.cs
@@ -72,6 +72,34 @@ public class BasketService
         return ApiResult<T>.Ok(responseView.Data);
     }
 
+    // TODO: Remove duplicate
+    public async UniTask<ApiResult<T>> GetActivitiesP<T>(
+        ActivityRequestParams @params,
+        CancellationToken token = default
+    )
+    {
+        // string url = $"{UrlBase}{@params.PlanId}/{@params.ChildId}";
+        //
+        // byte[] rawResponse = await HttpClientUnity.GetAsyncBytes(
+        //     url: url,
+        //     cancellationToken: token
+        // );
+        //
+        // using JsonDocument doc = JsonDocument.Parse(rawResponse);
+
+        using JsonDocument doc = JsonDocument.Parse(RootPrueba(ActivityType.Relate));
+        JsonElement root = doc.RootElement;
+
+        ApiResponseView<T> responseView = new(root);
+
+        if (!responseView.Success)
+        {
+            return ApiResult<T>.Fail(responseView.ErrorMessage);
+        }
+
+        return ApiResult<T>.Ok(responseView.Data);
+    }
+
     public string RootPrueba(ActivityType activityType = ActivityType.Judge)
     {
         string json =
@@ -206,11 +234,108 @@ public class BasketService
 }
 ";
 
+        string jsonRelate =
+            @"
+{
+    ""success"": true,
+    ""data"": {
+        ""activities"": [
+            {
+                ""main_word"": {
+                    ""word"": ""maça"",
+                    ""path"": ""IMG_0011.png"",
+                    ""id"": 2908,
+                    ""syllabified_word"": ""ma#ça"",
+                    ""word_sound"": ""sound00x.wav""
+                },
+                ""words"": [
+                    {
+                        ""word"": ""mamà"",
+                        ""path"": ""IMG_0105.png"",
+                        ""id"": 2910,
+                        ""syllabified_word"": ""ma#mà"",
+                        ""word_sound"": ""sound00x.wav"",
+                        ""answer"": true
+                    },
+                    {
+                        ""word"": ""xarop"",
+                        ""path"": ""IMG_2113.png"",
+                        ""id"": 4393,
+                        ""syllabified_word"": ""xa#rop"",
+                        ""word_sound"": ""sound00x.wav"",
+                        ""answer"": false
+                    },
+                    {
+                        ""word"": ""galta"",
+                        ""path"": ""IMG_2412.png"",
+                        ""id"": 5412,
+                        ""syllabified_word"": ""gal#ta"",
+                        ""word_sound"": ""sound00x.wav"",
+                        ""answer"": false
+                    },
+                    {
+                        ""word"": ""ballet"",
+                        ""path"": ""IMG_2435.png"",
+                        ""id"": 4525,
+                        ""syllabified_word"": ""ba#llet"",
+                        ""word_sound"": ""sound00x.wav"",
+                        ""answer"": false
+                    }
+                ]
+            },
+            {
+                ""main_word"": {
+                    ""word"": ""matí"",
+                    ""path"": ""IMG_0016.png"",
+                    ""id"": 2914,
+                    ""syllabified_word"": ""ma#tí"",
+                    ""word_sound"": ""sound00x.wav""
+                },
+                ""words"": [
+                    {
+                        ""word"": ""mare"",
+                        ""path"": ""IMG_0015.png"",
+                        ""id"": 2912,
+                        ""syllabified_word"": ""ma#re"",
+                        ""word_sound"": ""sound00x.wav"",
+                        ""answer"": true
+                    },
+                    {
+                        ""word"": ""saltar"",
+                        ""path"": ""IMG_1164.png"",
+                        ""id"": 4064,
+                        ""syllabified_word"": ""sal#tar"",
+                        ""word_sound"": ""sound00x.wav"",
+                        ""answer"": false
+                    },
+                    {
+                        ""word"": ""natja"",
+                        ""path"": ""IMG_1609.png"",
+                        ""id"": 3232,
+                        ""syllabified_word"": ""nat#ja"",
+                        ""word_sound"": ""sound00x.wav"",
+                        ""answer"": false
+                    },
+                    {
+                        ""word"": ""capa"",
+                        ""path"": ""IMG_2099.png"",
+                        ""id"": 5533,
+                        ""syllabified_word"": ""ca#pa"",
+                        ""word_sound"": ""sound00x.wav"",
+                        ""answer"": false
+                    }
+                ]
+            }
+        ]
+    }
+}
+";
+
         return activityType switch
         {
             ActivityType.Judge => json,
             ActivityType.Select => jsonSelect,
-            ActivityType.Relate => jsonSelect,
+            ActivityType.Relate => jsonRelate,
             _ => RootPrueba(),
         };
     }
