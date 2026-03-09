3272a64d1aacc81913ff9ab05a59df819634c437
RanMd <randommid1873@gmail.com>
2026-03-09 09:31:51 -0500

    chore(basket): add test scene

--- Picofon Home/Assets/Scripts/Core/Activities/BasketScripts/ProgressBar.cs
+++ Picofon Home/Assets/Scripts/Core/Activities/BasketScripts/ProgressBar.cs
@@ -6,24 +6,24 @@ public class ProgressBar : MonoBehaviour
     [SerializeField]
     private RectTransform fillRect;
 
-    private float _maxWidth;
-    private float _partWidth;
+    private float _maxHeight;
+    private float _partHeight;
 
     private Vector2 _size = Vector2.zero;
 
     public void Awake()
     {
-        _maxWidth = transform.GetComponent<RectTransform>().rect.width;
+        _maxHeight = transform.GetComponent<RectTransform>().rect.height;
     }
 
     public void Initialize(int parts)
     {
-        _partWidth = _maxWidth / parts;
+        _partHeight = _maxHeight / parts;
     }
 
     public void SetProgress(int progress)
     {
-        _size.x = _partWidth * progress;
+        _size.y = _partHeight * progress;
         Tween.UISizeDelta(fillRect, endValue: _size, duration: 0.5f);
     }
 }
