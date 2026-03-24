198590ef9e7b919916e549fa40291b322bb3d174
RanMd <randommid1873@gmail.com>
2026-03-24 11:06:49 -0500

    chore: update spline package

--- Picofon Home/Assets/Scripts/Core/MapPath/LevelPath.cs
+++ Picofon Home/Assets/Scripts/Core/MapPath/LevelPath.cs
@@ -1,5 +1,4 @@
 using System;
-using Cysharp.Threading.Tasks;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
@@ -25,13 +24,6 @@ public class LevelPath : MonoBehaviour
 
     public void ChangePath(Span<Vector2> points)
     {
-        Prueba(points.ToArray()).Forget();
-    }
-
-    private async UniTaskVoid Prueba(Vector2[] points)
-    {
-        await UniTask.WaitForSeconds(5f);
-
         SplineContainer _container = GetComponent<SplineContainer>();
         var spline = _container.Spline;
 
