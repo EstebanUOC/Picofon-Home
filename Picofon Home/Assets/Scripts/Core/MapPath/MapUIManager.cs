6fe23a6fa802ba5935d96361a876a82971a08cdc
RanMd <randommid1873@gmail.com>
2026-04-09 09:14:41 -0500

    chore: remove unused code in MapUIManager

--- Picofon Home/Assets/Scripts/Core/MapPath/MapUIManager.cs
+++ Picofon Home/Assets/Scripts/Core/MapPath/MapUIManager.cs
@@ -8,23 +8,11 @@ public class MapUIManager : MonoBehaviour
     [SerializeField]
     private SimpleButton _exitButton;
 
-    [SerializeField]
-    private UIResponsiveTransform _responsiveTransform;
-
     public void Start()
     {
         SceneOrientationHelper.LockToLandscape();
 
         _exitButton.OnClick += HandleExitButtonClicked;
-
-        if (SceneOrientationHelper.IsTablet())
-        {
-            PerformanceLog.Log(
-                $"Applying responsive transform for tablet: {_responsiveTransform.Scale}"
-            );
-            RectTransform target = _responsiveTransform.Target;
-            target.localScale = _responsiveTransform.Scale;
-        }
     }
 
     private void HandleExitButtonClicked()
