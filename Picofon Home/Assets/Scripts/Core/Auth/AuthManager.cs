69a338bbe011120a8f0eb1562c82fc0902c3f25f
RanMd <randommid1873@gmail.com>
2026-05-03 18:49:22 -0500

    fix(auth): fix the issue with user children panel

--- Picofon Home/Assets/Scripts/Core/Auth/AuthManager.cs
+++ Picofon Home/Assets/Scripts/Core/Auth/AuthManager.cs
@@ -227,29 +227,23 @@ public class AuthManager : MonoBehaviour
 
         if (CurrentUser == null)
         {
-            PerformanceLog.Log("No user session found, showing login panel.");
             _uiManager.Show(PanelEnum.Login, animate: false);
             return;
         }
 
         if (!_legalAccepted)
         {
-            PerformanceLog.Log("User has not accepted legal terms, showing disclaimer panel.");
             _uiManager.Show(PanelEnum.Disclaimer, animate: false);
             return;
         }
 
         if (CurrentUser.Role == UserRole.Invited)
         {
-            PerformanceLog.Log(
-                "User is invited but has not completed profile, showing role selection panel."
-            );
             _uiManager.Show(PanelEnum.Role, animate: false);
             return;
         }
 
-        PerformanceLog.Log("User session found, showing children panel.");
-        _uiManager.Show(PanelEnum.Children);
+        _uiManager.Show(PanelEnum.Children, animate: false);
     }
 
     private async UniTaskVoid BootAppProcess()
