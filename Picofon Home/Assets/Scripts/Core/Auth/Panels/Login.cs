f9122fefa8dd3d4e11a074c55ee26e1ec50fd492
RanMd <randommid1873@gmail.com>
2026-06-16 09:43:25 -0500

    chore(auth): implemented login functionality

--- Picofon Home/Assets/Scripts/Core/Auth/Panels/Login.cs
+++ Picofon Home/Assets/Scripts/Core/Auth/Panels/Login.cs
@@ -93,81 +93,6 @@ public class Login : MonoBehaviour
             firebaseIdToken
         );
 
-        if (result.Data.User.Role == UserRole.Therapist && !result.Data.User.ProfileCompleted)
-        {
-            ModalData modalData = new()
-            {
-                Title = "Profile Incomplete",
-                Message =
-                    "Your profile is incomplete. Please complete your profile in the web portal to access the app.",
-                Panel = _panel,
-            };
-
-            _loginButton.EndLoading();
-
-            await _uiManager.ShowModal(modalData);
-
-            _authManager.Logout();
-            return;
-        }
-
-        if (!result.Success)
-        {
-            ModalData modalData = new()
-            {
-                Title = "Error",
-                Message = "Could not log in. Please try again later.",
-                Panel = _panel,
-            };
-
-            _loginButton.EndLoading();
-
-            await _uiManager.ShowModal(modalData);
-
-            return;
-        }
-
-        _loginButton.EndLoading();
-
-        OnLoginSuccess(result.Data.User);
-    }
-
-    private void OnLoginSuccess(UserModel user)
-    {
-        _authManager.SetCurrentUser(user);
-        _loginButton.EndLoading();
-
-        if (!user.LegalAccepted)
-        {
-            _uiManager.ShowPanel(PanelEnum.Disclaimer);
-            return;
-        }
-
-        if (user.Role == UserRole.Invited)
-        {
-            _uiManager.ShowPanel(PanelEnum.Role);
-            return;
-        }
-
-        _uiManager.ShowPanel(PanelEnum.Children);
-    }
-
-    private void LoginWithGoogle()
-    {
-        // AuthenticateWithGoogle().Forget();
-
-        Prueba().Forget();
-    }
-
-    private async UniTaskVoid Prueba()
-    {
-        string firebaseIdToken =
-            "eyJhbGciOiJSUzI1NiIsImtpZCI6ImVlOTA0NmVhZDJlMDUwMDAxMGVkNTA0M2I0ODNkODRiMGM1MmM3YzQiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiRGFSa0FyIDUyMSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NJMTdQSnVuZWRrSU5La0NuNGlsRkJZbUs4RVFSSFg3T3cwM3BnaFpldm1tMkRJMGpuLT1zOTYtYyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9waWNvZm9uLTI4MmQ4IiwiYXVkIjoicGljb2Zvbi0yODJkOCIsImF1dGhfdGltZSI6MTc4MTM5Mjk2MCwidXNlcl9pZCI6Ik9sdkVUbjJub05PWWduUmF6SU42VFRIbzFjNTIiLCJzdWIiOiJPbHZFVG4ybm9OT1lnblJheklONlRUSG8xYzUyIiwiaWF0IjoxNzgxMzkyOTYwLCJleHAiOjE3ODEzOTY1NjAsImVtYWlsIjoibHVpc2FkYW5pZWw3MTdAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZ29vZ2xlLmNvbSI6WyIxMDY2MTQ5NzAxNTkwMTQ1NjI0NzUiXSwiZW1haWwiOlsibHVpc2FkYW5pZWw3MTdAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZ29vZ2xlLmNvbSJ9fQ.vfWnwfTxWkpgfukMJJnb1CXp4YfuHeQvFVCDxW69awPOCZKzMo6jnwsvxujm6Lt4K49srzsJJEoQLUHmVFcSkoRdPGLoJHXV-aYC3y7GAkQJ__dGvyr2Vrq64W68BV4kUJ7ClbPgQaPqT8xlyTNVJS04s9alUHTBa1_XF6Fq7hWojfaG_R5Tkiw4rOPEUuVOs_ii7wD3ALKRlm-OWUgmbk5jig27ZmeLrvV2g8jjHt96hN00eBa_dGomBrVyMzaNQ_xobbV16AbFOAKVvziQIKXFt-w8KtYx15byHp0zJ1Z60Q6XAjlT9iad03R1S9lL5MolUrGpATUlwyBecfPuNQ";
-
-        ApiResult<LoginData> result = await _authManager.UserService.LoginWithFirebaseToken(
-            firebaseIdToken
-        );
-
         if (!result.Success)
         {
             ModalData modalData = new()
@@ -214,6 +139,35 @@ public class Login : MonoBehaviour
             _authManager.Logout();
             return;
         }
+
+        _loginButton.EndLoading();
+
+        OnLoginSuccess(result.Data.User);
+    }
+
+    private void OnLoginSuccess(UserModel user)
+    {
+        _authManager.SetCurrentUser(user);
+        _loginButton.EndLoading();
+
+        if (!user.LegalAccepted)
+        {
+            _uiManager.ShowPanel(PanelEnum.Disclaimer);
+            return;
+        }
+
+        if (user.Role == UserRole.Invited)
+        {
+            _uiManager.ShowPanel(PanelEnum.Role);
+            return;
+        }
+
+        _uiManager.ShowPanel(PanelEnum.Children);
+    }
+
+    private void LoginWithGoogle()
+    {
+        AuthenticateWithGoogle().Forget();
     }
 
     private void ShowOptions()
