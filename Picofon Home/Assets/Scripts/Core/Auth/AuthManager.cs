2ed7c9301f636668ac281e71cca220cda919a881
RanMd <randommid1873@gmail.com>
2026-06-18 12:26:17 -0500

    test: userchildren

--- Picofon Home/Assets/Scripts/Core/Auth/AuthManager.cs
+++ Picofon Home/Assets/Scripts/Core/Auth/AuthManager.cs
@@ -107,7 +107,7 @@ public class AuthManager : MonoBehaviour
                     Id = "noXJSkWJnCW5iSEu32n5Kvofq5a2",
                     Email = "test@gmail.com",
                     Username = "Debug User",
-                    Role = UserRole.Admin,
+                    Role = UserRole.Therapist,
                 };
 
                 _initializeData.CurrentUser = user;
