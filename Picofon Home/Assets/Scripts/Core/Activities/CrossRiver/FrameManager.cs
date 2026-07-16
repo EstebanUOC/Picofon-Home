eb9809aafa7efb65c86f2c4e949056f45ef5964d
RanMd <randommid1873@gmail.com>
2026-07-16 13:00:56 -0500

    feat: add fade transition

--- Picofon Home/Assets/Scripts/Core/Activities/CrossRiver/FrameManager.cs
+++ Picofon Home/Assets/Scripts/Core/Activities/CrossRiver/FrameManager.cs
@@ -1,25 +1,50 @@
+using Cysharp.Threading.Tasks;
 using PrimeTween;
 using UnityEngine;
 
 public class FrameManager : MonoBehaviour
 {
-    public void Start() { }
+    // References
+
+    [SerializeField]
+    private Transform _container;
+
+    // Variables
+
+    public void Start()
+    {
+        ActivityRequestParams @params = LevelPayload.Params;
+
+#if DEBUG
+        if (@params.ChildId is null)
+        {
+            @params = new ActivityRequestParams { PlanId = 112, ChildId = "12345678Z" };
+
+            PerformanceLog.LogWarning("Using default parameters for testing in Unity Editor.");
+        }
+# endif
+    }
+
+    private async UniTaskVoid Prueba()
+    {
+        await UniTask.Delay(100);
+    }
 
     public void HideFrames()
     {
         float duration = 0.5f;
 
-        Tween.LocalPositionY(transform, endValue: 5, duration, ease: Ease.InBack);
+        Tween.LocalPositionY(_container, endValue: 5, duration, ease: Ease.InBack);
 
-        Tween.LocalPositionX(transform, endValue: -9.5f, duration: duration);
+        Tween.LocalPositionX(_container, endValue: -9.5f, duration: duration);
     }
 
     public void ShowFrames()
     {
-        transform.localPosition = new Vector3(0, 5, 0);
+        _container.localPosition = new Vector3(0, 5, 0);
 
         float duration = 0.5f;
 
-        Tween.LocalPositionY(transform, endValue: 0, duration, ease: Ease.OutBack);
+        Tween.LocalPositionY(_container, endValue: 0, duration, ease: Ease.OutBack);
     }
 }
