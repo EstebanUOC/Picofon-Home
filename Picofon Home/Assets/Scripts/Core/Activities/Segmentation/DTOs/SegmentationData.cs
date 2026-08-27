074a1bfcb3cd7119b301990084a8b5c6381eaf02
RndMD <randommid1873@gmail.com>
2026-08-27 13:48:37 -0500

    feat: add new segmentation data DTOs

---
+++ Picofon Home/Assets/Scripts/Core/Activities/Segmentation/DTOs/SegmentationData.cs
@@ -0,0 +1,25 @@
+namespace Picofon.Activities.Segmentation
+{
+    using System.Text.Json.Serialization;
+
+    public class SegmentationGeneralData
+    {
+        [JsonInclude]
+        public int SyllablesNumber { get; set; }
+
+        [JsonInclude]
+        public int SkillId { get; set; }
+
+        [JsonInclude]
+        public int TaskTypeId { get; set; }
+    }
+
+    public class SegmentationData
+    {
+        [JsonInclude]
+        public SegmentationGeneralData GeneralData { get; set; }
+
+        [JsonInclude]
+        public SegmentationActivity[] Activities { get; set; }
+    }
+}
