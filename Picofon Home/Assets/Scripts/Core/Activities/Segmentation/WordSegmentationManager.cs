f07e4297a548e459ec71753709272a03ab8ca70f
RndMD <randommid1873@gmail.com>
2026-08-28 10:33:56 -0500

    feat: add new papirus image

--- Picofon Home/Assets/Scripts/Core/Activities/Segmentation/WordSegmentationManager.cs
+++ Picofon Home/Assets/Scripts/Core/Activities/Segmentation/WordSegmentationManager.cs
@@ -1,12 +1,15 @@
 namespace Picofon.Activities.Segmentation
 {
     using Cysharp.Threading.Tasks;
+    using Picofon.Activities.Basket;
     using Picofon.Components;
     using Picofon.Core.MapPath;
     using Picofon.Core.Network;
     using Picofon.Utils;
+    using PrimeTween;
     using TMPro;
     using UnityEngine;
+    using UnityEngine.SceneManagement;
 
     public class WordSegmentationManager : MonoBehaviour
     {
@@ -27,6 +30,32 @@ namespace Picofon.Activities.Segmentation
         [SerializeField]
         private TMP_Text _wordText;
 
+        [Space]
+        [SerializeField]
+        private GameMenu _gameMenu;
+
+        [SerializeField]
+        private Fade _fade;
+
+        [SerializeField]
+        private ProgressBar _progressBar;
+
+        [SerializeField]
+        private Counter _counter;
+
+        [Space]
+        [SerializeField]
+        private RectTransform _progressBarTransform;
+
+        [SerializeField]
+        private RectTransform _counterTransform;
+
+        [SerializeField]
+        private RectTransform _menuTransform;
+
+        [SerializeField]
+        private RectTransform _labelTransform;
+
         #endregion
 
         // Variables
@@ -41,6 +70,10 @@ namespace Picofon.Activities.Segmentation
 
         private bool _expectedAnswer;
 
+        private float _defaultMenuX;
+        private float _defaultCounterX;
+        private float _defaultProgressBarValue;
+
         private static readonly System.Random _rng = new();
 
         public void Awake()
@@ -49,6 +82,8 @@ namespace Picofon.Activities.Segmentation
 
             _yesButton.OnClick += () => HandleAnswer(true);
             _noButton.OnClick += () => HandleAnswer(false);
+
+            _gameMenu.OnMenuOptionSelected += HandleMenuOptionSelected;
         }
 
         public async void Start()
@@ -70,17 +105,27 @@ namespace Picofon.Activities.Segmentation
 
         private async UniTask LoadActivities(ActivityRequestParams @params)
         {
+            await UniTask.WaitForEndOfFrame(this);
+
+            _fade.FirstLoad();
+
+            PositionMenu();
+
             ApiResult<SegmentationData> result = await _dataManager.LoadActivities(@params);
 
             if (!result.Success)
             {
                 Debug.LogError($"[WordSegmentation] Error loading activities: {result.Message}");
+
+                _fade.StopAndZoom();
                 return;
             }
 
             if (!_dataManager.HasActivities())
             {
                 Debug.LogWarning("[WordSegmentation] No activities found.");
+
+                _fade.StopAndZoom();
                 return;
             }
 
@@ -90,7 +135,117 @@ namespace Picofon.Activities.Segmentation
                 $"[WordSegmentation] Loaded {_dataManager.GetActivityCount()} activities successfully."
             );
 
+            _progressBar.Initialize(_dataManager.GetActivityCount(), false);
+
+            _fade.StopAndZoom();
+            AnimateUI().Forget();
+
             SetupRound();
+        }
+
+        private void PositionMenu()
+        {
+            _defaultMenuX = _menuTransform.anchoredPosition.x;
+            _defaultCounterX = _counterTransform.anchoredPosition.x;
+
+            _menuTransform.anchoredPosition = new Vector2(-200, _menuTransform.anchoredPosition.y);
+            _counterTransform.anchoredPosition = new Vector2(
+                400,
+                _counterTransform.anchoredPosition.y
+            );
+
+            if (_progressBarTransform.rotation.z == 0)
+            {
+                _defaultProgressBarValue = _progressBarTransform.anchoredPosition.y;
+
+                _progressBarTransform.anchoredPosition = new Vector2(
+                    _progressBarTransform.anchoredPosition.x,
+                    700
+                );
+            }
+            else
+            {
+                _defaultProgressBarValue = _progressBarTransform.anchoredPosition.x;
+
+                _progressBarTransform.anchoredPosition = new Vector2(
+                    -100,
+                    _progressBarTransform.anchoredPosition.y
+                );
+            }
+        }
+
+        private async UniTaskVoid AnimateUI()
+        {
+            await UniTask.WaitForSeconds(1f);
+
+            _ = Tween.UIAnchoredPositionX(
+                target: _menuTransform,
+                endValue: _defaultMenuX,
+                duration: 0.5f,
+                ease: Ease.OutCubic
+            );
+
+            _ = Tween.UIAnchoredPositionX(
+                target: _counterTransform,
+                endValue: _defaultCounterX,
+                duration: 0.5f,
+                ease: Ease.OutCubic
+            );
+
+            if (_progressBarTransform.rotation.z == 0)
+            {
+                _ = Tween.UIAnchoredPositionY(
+                    target: _progressBarTransform,
+                    endValue: _defaultProgressBarValue,
+                    duration: 0.5f,
+                    ease: Ease.OutCubic
+                );
+            }
+            else
+            {
+                _ = Tween.UIAnchoredPositionX(
+                    target: _progressBarTransform,
+                    endValue: _defaultProgressBarValue,
+                    duration: 0.5f,
+                    ease: Ease.OutCubic
+                );
+            }
+        }
+
+        private void HideUI()
+        {
+            _ = Tween.UIAnchoredPositionX(
+                target: _menuTransform,
+                endValue: -200,
+                duration: 0.5f,
+                ease: Ease.OutCubic
+            );
+
+            _ = Tween.UIAnchoredPositionX(
+                target: _counterTransform,
+                endValue: 400,
+                duration: 0.5f,
+                ease: Ease.OutCubic
+            );
+
+            if (_progressBarTransform.rotation.z == 0)
+            {
+                _ = Tween.UIAnchoredPositionY(
+                    target: _progressBarTransform,
+                    endValue: 700,
+                    duration: 0.5f,
+                    ease: Ease.OutCubic
+                );
+            }
+            else
+            {
+                _ = Tween.UIAnchoredPositionX(
+                    target: _progressBarTransform,
+                    endValue: -100,
+                    duration: 0.5f,
+                    ease: Ease.OutCubic
+                );
+            }
         }
 
         private void SetupRound()
@@ -116,6 +271,9 @@ namespace Picofon.Activities.Segmentation
                 $"[WordSegmentation] Word=\"{_currentActivity.Word.Word}\", Syllables={_syllablesNumber}, Fingers={_currentFingers}, Expected={(_expectedAnswer ? "Yes" : "No")}, Selected={(isYes ? "Yes" : "No")} → {(isCorrect ? "CORRECT" : "INCORRECT")}"
             );
 
+            _progressBar.SetProgress(_dataManager.GetCurrentIndex() + 1, isCorrect);
+            _counter.AddScore(isCorrect);
+
             if (_dataManager.MoveNext())
             {
                 SetupRound();
@@ -123,7 +281,36 @@ namespace Picofon.Activities.Segmentation
             else
             {
                 Debug.Log("[WordSegmentation] All activities completed.");
+
+                HideUI();
+
+                _fade.Load();
+
+                LoadScene("MapPathScene").Forget();
             }
+        }
+
+        private void HandleMenuOptionSelected(GameMenuEvent menuEvent)
+        {
+            switch (menuEvent)
+            {
+                case GameMenuEvent.Exit:
+                    LoadScene("MapPathScene").Forget();
+                    break;
+            }
+        }
+
+        private async UniTask LoadScene(string sceneName)
+        {
+            AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneName);
+            loadOperation.allowSceneActivation = false;
+
+            await UniTask.WaitUntil(() => loadOperation.progress >= 0.9f);
+
+            _ = _fade.Stop(
+                target: loadOperation,
+                onComplete: target => target.allowSceneActivation = true
+            );
         }
 
         private Sprite LoadSprite(string path)
